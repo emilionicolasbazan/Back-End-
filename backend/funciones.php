e7abaf1125696fd4ec59ca58105bbd872609569c
Emilio Bazan <83828888+emilionicolasbazan@users.noreply.github.com>
2021-09-22 10:21:56 -0300

    Add files via upload

---
+++ backend/funciones.php
@@ -0,0 +1,20 @@
+<?php
+    $productos = [  
+      ['nombre' => 'Pizza', 'precio' => 800 , 'imagen' => 'img/pizza.png',],
+      ['nombre' => 'Hamburguesa', 'precio' => 670 , 'imagen' => 'img/hamburguesa.png',],
+      ['nombre' => 'Sandwich', 'precio' => 450 , 'imagen' => 'img/sandwich.png',],
+      ['nombre' => 'Pancho', 'precio' => 250 , 'imagen' => 'img/pancho.png',],
+      ['nombre' => 'Papas Fritas', 'precio' => 300 , 'imagen' => 'img/papas-fritas.png',],
+      ['nombre' => 'Ensalada', 'precio' => 170 , 'imagen' => 'img/ensalada.png',],
+      ['nombre' => 'Galletitas', 'precio' => 140 , 'imagen' => 'img/galletitas.png',],
+      ['nombre' => 'Dona', 'precio' => 110 , 'imagen' => 'img/dona.png',],
+    ]
+?>
+
+
+
+
+
+
+
+
